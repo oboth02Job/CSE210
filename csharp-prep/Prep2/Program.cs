813df76198896c906ee3499ddf33f717926cb75d
Job Oboth <trappsavage601@gmail.com>
2024-10-31 19:54:17 +0300

    finished prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,51 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine(" What is your grade percentage? ");
+        string answer = Console.ReadLine();
+        int percent = int.Parse(answer);
+        string letter = "";
+
+        //Determine the letter grade
+        if (percent >= 90)
+        {
+         letter = "A";
+
+        }
+        else if (percent >= 80)
+        {
+            letter = "B";
+            
+        }
+        else if (percent >= 70) 
+        {
+            letter = "C";
+            
+            
+        }
+        else if (percent >= 60)
+        {
+            letter = "D";
+            
+            }
+
+            else
+            {
+                letter = "F";
+             
+             }  
+
+             //print the letter grade
+             Console.WriteLine($"Your grade is: {letter}");
+        //Check if the user passed the course
+        if (percent >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the course");
+        }
+        else 
+        {
+            Console.WriteLine("Keep trying and don't lose hope!");
+        }
+
     }
 }
